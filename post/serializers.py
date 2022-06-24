e2d89b0757b29ff07e8cfa6c7ef5d6aeea2c34f4
Daegeun Jeong <wjdeorms27@naver.com>
2022-06-24 18:33:54 +0900

    Feat : modify serializers and views

--- post/serializers.py
+++ post/serializers.py
@@ -5,22 +5,51 @@ from post.models import Company
 from .models import Company
 from .models import JobPost
 from .models import JobPostSkillSet
+from .models import JobType
+
+class JobTypeSerializer(serializers.ModelSerializer):
+    """
+    JobType 모델에서 id 값과 job_type을 가져온다.
+    """
+    
+    class Meta:
+        model = JobType
+        fields = ["id", "job_type"]
 
 class CompanySerializer(serializers.ModelSerializer):
+    """
+    Company 모델에서 id 값과 company_name을 가져온다.
+    """
+    
     class Meta:
         model = Company
-        fields = ["company_name", "business_area"]
+        fields = ["id", "company_name"]
         
 class JobPostSerializer(serializers.ModelSerializer):
-    company = serializers.SerializerMethodField()
+    """
+    1. job_type과 company는 ForeignKey 
+    2. job_description과 salary는 JobPost 모델에 있는 정보
     
-    def get_company(self, obj):
-        print(dir(obj))
-        return obj.company_name
+    1. company는 회사의 정보를 다른 테이블에서 가져와 정보를 보여주는 것이므로 read_only 
+       -> id와 company_name을 보여줌
+    2. job_type 테이블에서는 job_type이라는 정보를 가져올 수 있음
+       -> JobPost에서 JobType을 바라보고 있으므로 정참조를 사용하면 된다. 
+    """
+    company = CompanySerializer(read_only=True)
+    # job_type = JobTypeSerializer(read_only=True)
+    skillsets = serializers.SerializerMethodField()
+    job_type = serializers.SerializerMethodField()
+        
+    def get_job_type(self, obj):
+        return obj.job_type.job_type
+    
+    def get_skillsets(self, obj):
+        print(obj.skillset_set.all())
+        return [s.name for s in obj.skillset_set.all()]
     
     class Meta:
         model = JobPost
-        fields = ["job_type", "company", "job_description", "salary"]
+        fields = ["id", "job_type", "company", "job_description", "salary","skillsets"]
         
         
 class JobPostSkillSetSerializer(serializers.ModelSerializer):
--- post/views.py
+++ post/views.py
@@ -19,24 +19,59 @@ class SkillView(APIView):
     def get(self, request):
         skills = self.request.query_params.getlist('skills', '')
         print("skills = ", end=""), print(skills)
-
+        
+        query = Q()
+        for skill in skills:
+            query.add(Q(skill_set__name=skill), Q.OR)
+        
+        job_skills = JobPostSkillSet.objects.filter(query)
+        job_posts = JobPost.objects.filter(
+            id__in=[job_skill.job_post.id for job_skill in job_skills]
+        )
+        
+        if job_posts.exists():
+            serialized_posts = JobPostSerializer(job_posts, many=True)
+            return Response(serialized_posts.data)
+        
         return Response(status=status.HTTP_200_OK)
 
 
 class JobView(APIView):
 
     def post(self, request):
-        job_type = int( request.data.get("job_type", None) )
-        company_name = request.data.get("company_name", None)
-        request.data["job_type"] = job_type
-
+        """
+        * exists를 사용하려면 filter로 값을 받아와야 함
+        
+        1. job_type이 만약 테이블에 없다면 400 에러 메세지
+           -> 먼저 JobType 모델에 해당 값이 존재하는지 찾아보자.
+           -> find_job_type으로 찾고, 결과가 나오면 JobType object (1) 또는 JobType object (2)로 나옴
+        2. 회사 이름이 존재 하지 않으면 생성해서 채용공고를 등록
+           -> 회사 이름이 존재하는지 찾아보자. 
+           -> 만약 회사 이름이 없다면 <QuerySet []>이 반환됨
+           -> 회사 이름이 없다면 이름을 채워 저장하고, 있다면 해당하는 이름 중 첫번째 값 가져옴
+        3. JobPostSerializer에 수정된 데이터를 넣음
+        4. 그리고 is_valid()를 통해 데이터가 형식에 맞게, 필드에 맞게 잘 들어왔다면
+           저장하고, 아니라면 에러 메세지 보내줌
+        """
+        # 1
+        job_type = int(request.data.get("job_type", None))
+        find_job_type = JobType.objects.filter(id=job_type)
+        if not find_job_type.exists():
+            return Response({"error": "invalid job type"}, status=status.HTTP_400_BAD_REQUEST)
+ 
+        # 2
+        company_name = request.data.get("company", None)
+        company = Company.objects.filter(company_name=company_name)
+        if not company.exists():
+            company = Company(company_name=company_name).save()
+        else:
+            company = company.first()
+        
         serialized_post = JobPostSerializer(data=request.data)
+
         
         if serialized_post.is_valid():
-            # if not request.data["company_name"]:
-            #     new_company = Company.objects.create(company_name=request.data["company_name"], business_area="none")
-            #     new_company.save()
-            #     JobPost.company.add(new_company)
+            serialized_post.save(company=company, job_type=find_job_type.first())
             return Response(serialized_post.data, status=status.HTTP_200_OK)
         
         return Response(serialized_post.errors, status=status.HTTP_400_BAD_REQUEST)
